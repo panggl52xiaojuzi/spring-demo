9465f4c1735de0ff8d74680c16bb8e8d274de02f
jinxun.ljx <jinxun.ljx@alibaba-inc.com>
2021-08-09 11:28:03 +0800

    Springboot 演示程序

---
+++ src/main/java/com/alibabacloud/web/WebApplication.java
@@ -0,0 +1,21 @@
+package com.alibabacloud.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
+    public String hello(){
+        return "hello mingyu";
+    }
+
+}
